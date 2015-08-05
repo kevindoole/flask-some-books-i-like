7dd3c59dc8f5892d102f88593938c325328b5de2
kevindoole <kdoole@gmail.com>
2015-08-05 00:40:20 -0500

    refactor: Abstract auth checks in admin views into a decorator

--- catalog/cat_app/views/admin.py
+++ catalog/cat_app/views/admin.py
@@ -1,17 +1,23 @@
 from flask import render_template, request, flash, redirect, url_for, Blueprint
+from functools import wraps
 from cat_app import app, db, login_session
 from cat_app.models import Product, Category
 from cat_app.form_requests.product import ProductForm
 
 admin = Blueprint('admin', __name__)
 
+def login_required(f):
+    @wraps(f)
+    def protected_route(*args, **kwargs):
+        if 'username' not in login_session:
+            flash('You are not authorized to access that page. Please log in.')
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return protected_route
 
 @admin.route('/catalog/create-product', methods=['GET', 'POST'])
+@login_required
 def new_product():
-    if 'username' not in login_session:
-        flash('You are not authorized to access that page. Please log in.')
-        return redirect('/login')
-
     form = ProductForm(request.form)
     if request.method == 'POST' and form.validate():
         category_name = form.category.data
@@ -36,10 +42,8 @@ def new_product():
 
 
 @admin.route('/catalog/<string:product_slug>/edit', methods=['GET', 'POST'])
+@login_required
 def edit_product(product_slug):
-    if 'username' not in login_session:
-        flash('You are not authorized to access that page. Please log in.')
-        return redirect('/login')
     product = Product.query.filter(Product.slug == product_slug).one()
     form = ProductForm(request.form, product)
     if request.method == 'POST' and form.validate():
@@ -68,10 +72,8 @@ def edit_product(product_slug):
 
 
 @admin.route('/catalog/<string:product_slug>/delete', methods=['GET', 'POST'])
+@login_required
 def delete_product(product_slug):
-    if 'username' not in login_session:
-        flash('You are not authorized to access that page. Please log in.')
-        return redirect('/login')
     product = Product.query.filter(Product.slug == product_slug).one()
     if request.method == 'POST':
         db.session.delete(product)
